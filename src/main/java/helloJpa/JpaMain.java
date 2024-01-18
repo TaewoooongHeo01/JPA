7dd9806c482a25095865faa4ac03b924b1fc2afa
TaewoooongHeo01 <chamjoeun0111@gmail.com>
2024-01-18 16:39:31 +0900

    first commit

---
+++ src/main/java/helloJpa/JpaMain.java
@@ -0,0 +1,9 @@
+package helloJpa;
+
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        Persistence.createEntityManagerFactory("hello");
+    }
+}
